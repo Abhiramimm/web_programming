ccc662de6920b10b880700e5cf1d9aa33c0c705d
Abhiramimm <74661724+Abhiramimm@users.noreply.github.com>
2021-03-01 16:00:33 +0530

    Add files via upload

---
+++ maxn.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<title>Max out of three</title>
+</head>
+<body>
+<?php
+$a = 1;
+$b  =4;
+$c = 3;
+if($a >$b)
+{
+if($a > $c)
+{
+echo "maximum num a = $a";
+else
+echo "maximum num c = $c";
+}
+else
+{
+if($c > $b)
+{
+echo "echo "maximum num c = $c";
+else
+echo "maximum num b = $b";
+}
+?>
+</body>
+</html>
